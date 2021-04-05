8679d504a1629306d397d37d3842b95f8b03abca
kassiopatrik@outlook.com <kassiopatrik@outlook.com>
2021-04-04 22:21:06 -0300

    Adicionando verbose_names nas classes de vendas

--- vendas/models.py
+++ vendas/models.py
@@ -4,19 +4,20 @@ from django.db import models
 # Create your models here.
 
 class Vendas(models.Model):
-    nome = models.CharField(max_length=255, null=False, blank=False)
-    valor = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False)
+    nome = models.CharField(max_length=255, null=False, blank=False, verbose_name='Nome')
+    valor = models.DecimalField(max_digits=12, decimal_places=2, null=False, blank=False,
+                                verbose_name='Valor Total da Venda')
     data_venda = models.DateField(auto_now_add=True, blank=True, null=False)
     hora_venda = models.TimeField(auto_now_add=True, blank=True, null=False)
     data_hora_venda = models.DateTimeField(auto_now_add=True, blank=True, null=False)
-    numero_venda = models.IntegerField(blank=False, null=False)
-    observacao = models.TextField(blank=True, null=True)
-    comprovante_venda = models.FileField(upload_to='Comprovante_venda/')
-    email_cliente = models.EmailField(blank=True, null=True)
-    venda_concluida = models.BooleanField(blank=False, null=False)
-    qtd_itens = models.IntegerField(blank=True, null=False, default=0)
+    numero_venda = models.IntegerField(blank=False, null=False, verbose_name='Número da venda')
+    observacao = models.TextField(blank=True, null=True, verbose_name='Observação')
+    comprovante_venda = models.FileField(upload_to='Comprovante_venda/', verbose_name='Comprovante de Venda')
+    venda_concluida = models.BooleanField(blank=False, null=False, verbose_name='Venda Concluída')
+    qtd_itens = models.IntegerField(blank=True, null=False, default=0, verbose_name='Quantidade de Itens Vendidos')
     produtos = models.ManyToManyField('Produto')
-    cliente = models.ForeignKey('Cliente', on_delete=models.DO_NOTHING, default=1)
+    cliente = models.ForeignKey('Cliente', on_delete=models.DO_NOTHING, default=1, verbose_name='Cliente')
+
 
     def __str__(self):
         return str(self.pk) + ' - ' + self.nome
@@ -31,8 +32,10 @@ class Produto(models.Model):
 
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=255, blank=False, null=False)
-    cpf = models.CharField(max_length=11, blank=False, null=False)
+    nome = models.CharField(max_length=255, blank=False, null=False, verbose_name='Nome Completo')
+    sobrenome = models.CharField
+    cpf = models.CharField(max_length=11, blank=False, null=False, verbose_name='CPF')
+    email_cliente = models.EmailField(blank=True, null=True, verbose_name='E-mail')
 
     def __str__(self):
         return self.nome
